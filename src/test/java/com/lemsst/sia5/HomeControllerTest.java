92af423a326a2445d05918033cff78b2bfb8edcf
Lemuel Bulangis <lemgrb@gmail.com>
2019-03-23 14:02:57 +0800

    Chapter 1

---
+++ src/test/java/com/lemsst/sia5/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.lemsst.sia5;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testGetHomepage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.view().name("home"))
+		.andExpect(MockMvcResultMatchers.content().string(containsString("Hello world")));
+			
+	}
+	
+}
