380b678a5d211ef394d4972c827fd2058eac0d92
Lemuel Bulangis <lemgrb@gmail.com>
2019-03-23 17:47:28 +0800

    Chapter 2 | Without validations. It's giving me error! haha

---
+++ src/test/java/com/lemsst/sia5/controller/DesignTacoControllerTest.java
@@ -0,0 +1,31 @@
+package com.lemsst.sia5.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.lemsst.sia5.HomeController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class DesignTacoControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testShowDesignForm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessDesign() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/lemsst/sia5/data/OrderTest.java
@@ -0,0 +1,14 @@
+package com.lemsst.sia5.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
