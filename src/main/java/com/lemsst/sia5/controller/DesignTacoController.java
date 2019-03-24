9c12a15ad1316acb77fadab0b438b5744400eee3
Lemuel Bulangis <lemgrb@gmail.com>
2019-03-24 12:23:39 +0800

    Add validation in DesignTaco page and add bootstrap

--- src/main/java/com/lemsst/sia5/controller/DesignTacoController.java
+++ src/main/java/com/lemsst/sia5/controller/DesignTacoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,8 +25,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-	@GetMapping
-	public String showDesignForm(Model model) {
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
 		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
 				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
 				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
@@ -33,28 +34,29 @@ public class DesignTacoController {
 				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
 				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 
-		log.info("Some weird shit");
-
 		Type[] types = Ingredient.Type.values();
 
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
-
-		model.addAttribute("design", new Taco());
-
-		return "design";
 	}
 
+	@GetMapping
+	public String showDesignForm(Model model) {
+		model.addAttribute("design", new Taco());
+		return "design";
+	}
+	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
 		return ingredients.stream().filter(i -> i.getType().equals(type)).collect(Collectors.toList());
 	}
 	
 	@PostMapping
-	public String processDesign(Taco design, Errors errors) {
+	public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+		log.info("The design is : " + design);
 		
 		if(errors.hasErrors()) {
-			log.info("Process design errors");
+			log.info("Process design errors: " + errors);
 			return "design";
 		}
 		// Save the taco design
